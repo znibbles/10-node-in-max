ce2c726a51ea5722b225444cd824ae4b149b6450
Julian Rubisch <julian@julianrubisch.at>
2018-09-30 20:30:04 +0200

    Remove comments #1

--- 1-watch-youtube-videos/watch-youtube.js
+++ 1-watch-youtube-videos/watch-youtube.js
@@ -2,16 +2,7 @@ const maxApi = require("max-api");
 const ytdl = require('ytdl-core');
 
 maxApi.addHandler("open", (url) => {
-	// const url = `${BASE_URL}breed/${name}/images/random`;
-	// request(url).then((data) => {
-	// 	if (data.status === "success") {
-	// 		maxApi.outlet("dog", data.message);
-	// 	} else {
-	// 		maxApi.post("Error fetching dog image: " + data.message);
-	// 	}
-  // });
-  
-  ytdl.getInfo(url)
+	ytdl.getInfo(url)
     .then(info => {
       let format = ytdl.chooseFormat(info.formats, {});
       maxApi.outlet("download_url", format.url);
